4d59a779bf76e1c3a42a0c9ff7966f311e6e1f5c
Faheem <faheemajmal2014@gmail.com>
2025-09-27 13:19:46 +0500

    Create TransferForm.vue

---
+++ resources/js/pages/components/TransferForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h3>Send Money</h3>
+        <form @submit.prevent="submit">
+            <div>
+                <label>Receiver ID</label>
+                <input v-model.number="receiverId" type="number" />
+            </div>
+            <div>
+                <label>Amount</label>
+                <input v-model.number="amount" type="number" step="0.01" />
+            </div>
+            <button :disabled="loading">{{ loading ? 'Sending...' : 'Send' }}</button>
+            <div v-if="error" style="color:red;margin-top:10px">{{ error }}</div>
+            <div v-if="success" style="color:green;margin-top:10px">{{ success }}</div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+const props = defineProps({
+    userId: Number
+});
+const emit = defineEmits(['transfer-success']);
+
+const receiverId = ref('');
+const amount = ref('');
+const loading = ref(false);
+const error = ref('');
+const success = ref('');
+
+async function submit() {
+    error.value = '';
+    success.value = '';
+    loading.value = true;
+    try {
+        const resp = await axios.post('/api/transactions', {
+            receiver_id: receiverId,
+            amount: amount
+        });
+        success.value = 'Transfer succeeded';
+        emit('transfer-success');
+    } catch (e) {
+        if (e.response?.data?.errors) {
+            error.value = Object.values(e.response.data.errors).flat().join(', ');
+        } else if (e.response?.data?.message) {
+            error.value = e.response.data.message;
+        } else {
+            error.value = 'An error occurred';
+        }
+    } finally {
+        loading.value = false;
+    }
+}
+</script>

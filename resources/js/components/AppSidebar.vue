22592441c4a1b9f7c5f96601431975d6ca2e19de
Faheem <faheemajmal2014@gmail.com>
2025-09-27 11:51:36 +0500

    UI tweaks

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -28,13 +28,8 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/faheem2016/mini-wallet',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
     },
 ];
 </script>
